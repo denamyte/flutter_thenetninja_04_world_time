541dd80ab56a43f2f38efcfb0b34a01330edd813
denamyte <denamyte@gmail.com>
2021-03-30 02:02:58 +0300

    Lesson 23: Dart maps; Routing in a Futter application

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:thenetninja_04_world_time/pages/choose_location.dart';
 import 'package:thenetninja_04_world_time/pages/home.dart';
+import 'package:thenetninja_04_world_time/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  // home: Home(),
+  initialRoute: "/home",
+  routes: {
+    "/": (context) => Loading(),
+    "/home": (context) => Home(),
+    "/location": (context) => ChooseLocation(),
+
+  },
 ));
 
