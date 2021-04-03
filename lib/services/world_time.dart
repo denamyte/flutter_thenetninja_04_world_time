7a22d0850f0039f3a15a54e33446b797d67f13e0
denamyte <denamyte@gmail.com>
2021-04-04 00:43:23 +0300

    Lesson 28: Custom WorldTime class: incapsulaiting logic

---
+++ lib/services/world_time.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WorldTime {
+
+  String location;  // location name for the UI
+  String time;  // the time in that location
+  String flag;  // URL to an asset flag icon
+  String url;  // location URL for API endpoint
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+
+    // http://worldtimeapi.org/api/timezone/Europe/London
+    http.Response response = await http.get(Uri.https('worldtimeapi.org', '/api/timezone/$url'));
+    Map<String, dynamic> data = jsonDecode(response.body);
+    // print(data);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    // print(datetime);
+    // print(offset);
+
+    DateTime now = DateTime.parse(datetime).add(Duration(hours: int.parse(offset)));
+    // now = now.add(Duration(hours: int.parse(offset)));
+
+    // set the time property
+    time = now.toString();
+
+  }
+
+}
